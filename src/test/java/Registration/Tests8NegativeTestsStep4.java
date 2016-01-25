0624d41c1e53cb9da835750593965c9862159427
vladislav911 <vlad_kovtun_911@mail.ru>
2016-01-25 18:14:29 +0200

    Step4

---
+++ src/test/java/Registration/Tests8NegativeTestsStep4.java
@@ -0,0 +1,42 @@
+package Registration;
+import org.omg.CORBA.Current;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.*;
+import static org.testng.Assert.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.Select;
+
+public class Tests8NegativeTestsStep4 extends TestDataBase {
+    @Test(priority = 1)
+    public void TryToRegistrationWithoutOmfang() throws Exception {
+        // Go to Page Step4
+        driver.get(UrlPageStep4);
+        // Chose Ønsket jobbtype  - Fast ansettelse
+        driver.findElement(By.cssSelector("div.empCnt > label.checkbox-label")).click();
+        // Get current Url1
+        String Url1 = driver.getCurrentUrl();
+        // Click button Registration
+        driver.findElement(By.xpath("//*[@id='registration_form']/div/div[2]/div[1]/div/div[5]/div/button")).click();
+        // Get current Url2
+        String Url2 = driver.getCurrentUrl();
+        // Verify Url1=Url2
+        Assert.assertEquals(Url1, Url2);
+    }
+    @Test(priority = 2)
+    public void TryToRegistrationWithoutJobbtype() throws Exception {
+        // Clean Ønsket jobbtype  - Fast ansettelse
+        driver.findElement(By.cssSelector("div.empCnt > label.checkbox-label")).click();
+        // Chose Ønsket omfang  - Heltid
+        driver.findElement(By.cssSelector("label.checkbox-label")).click();
+        // Get current Url1
+        String Url1 = driver.getCurrentUrl();
+        // Click button Registration
+        driver.findElement(By.xpath("//*[@id='registration_form']/div/div[2]/div[1]/div/div[5]/div/button")).click();
+        // Get current Url2
+        String Url2 = driver.getCurrentUrl();
+        // Verify Url1=Url2
+        Assert.assertEquals(Url1, Url2);
+    }
+}

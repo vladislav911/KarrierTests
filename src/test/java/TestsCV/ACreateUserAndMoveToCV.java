662a8dd97ec7018be2a3c87a3021e9704dc55a5e
vladislav911 <vlad_kovtun_911@mail.ru>
2016-02-03 18:51:27 +0200

    confirm registration

---
+++ src/test/java/TestsCV/ACreateUserAndMoveToCV.java
@@ -0,0 +1,189 @@
+package TestsCV;
+import com.thoughtworks.selenium.Selenium;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.*;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import static org.testng.Assert.*;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.testng.Assert.fail;
+
+import org.testng.annotations.Test;
+
+public class ACreateUserAndMoveToCV extends TestBaseCV {
+    @Test
+    public void CreateUser() throws Exception {
+        // Go to http://karrierestart.no/registrering
+        driver.get(baseUrl + "/registrering");
+        // Type Emai
+        driver.findElement(By.id("Email")).clear();
+        driver.findElement(By.id("Email")).sendKeys(Email);
+        // Type Password
+        driver.findElement(By.id("RegistratePassword")).clear();
+        driver.findElement(By.id("RegistratePassword")).sendKeys("test");
+        // Type ConfirmPassword
+        driver.findElement(By.id("ConfirmPassword")).clear();
+        driver.findElement(By.id("ConfirmPassword")).sendKeys("test");
+        // Click checkbox
+        driver.findElement(By.className("checkbox-label")).click();
+        // click Registration button
+        driver.findElement(By.id("submit")).click();
+        // Step2
+        // Type First Name
+        driver.findElement(By.id("FirstName")).sendKeys("Test3");
+        // Type Last Name
+        driver.findElement(By.id("LastName")).clear();
+        driver.findElement(By.id("LastName")).sendKeys("User3");
+        // Chose day
+        Select selectByValueDay = new Select(driver.findElement(By.className("day")));
+        selectByValueDay.selectByVisibleText("07");
+        // Chose month
+        Select selectByValueMonth = new Select(driver.findElement(By.className("month")));
+        selectByValueMonth.selectByVisibleText("mai");
+        // Chose year
+        Select selectByValueYear = new Select(driver.findElement(By.className("year")));
+        selectByValueYear.selectByVisibleText("1989");
+        // Scroll page up
+        ((JavascriptExecutor) driver).executeScript("window.scrollTo(0, document.head.scrollHeight)");
+        // Сhose gender
+        driver.findElement(By.id("male")).click();
+        // Click button Registration
+        ClickOnButtonRegistrationStep2();
+        // Step3
+        // Type Adresse
+        driver.findElement(By.id("StreetAddress")).clear();
+        driver.findElement(By.id("StreetAddress")).sendKeys("Gagarina street");
+        // Type Postnummer
+        driver.findElement(By.id("ZipCode")).clear();
+        driver.findElement(By.id("ZipCode")).sendKeys("42121");
+        // Type Sted
+        driver.findElement(By.id("Location")).clear();
+        driver.findElement(By.id("Location")).sendKeys("Kiev");
+        // Type Mobiltelefon
+        driver.findElement(By.id("CellPhone")).clear();
+        driver.findElement(By.className("selected-flag")).click();
+        driver.findElement(By.cssSelector(".iti-flag.ua")).click();
+        driver.findElement(By.id("StreetAddress")).click();
+        driver.findElement(By.id("CellPhone")).sendKeys("938872305");
+        driver.findElement(By.id("StreetAddress")).click();
+        // Chose Land
+        driver.findElement(By.id("Country_chosen")).click();
+        driver.findElement(By.xpath("//*[@id='Country_chosen']/div/div/input")).sendKeys("Ukraina");
+        driver.findElement(By.xpath("//*[@id='Country_chosen']/div/ul/li/em")).click();
+        driver.findElement(By.id("StreetAddress")).click();
+        String Url1 = driver.getCurrentUrl();
+        // Click button Registration
+        ClickOnButtonRegistrationStep3();
+        // Step4
+        // Chose Ønsket jobbtype  - Fast ansettelse
+        driver.findElement(By.cssSelector("div.empCnt > label.checkbox-label")).click();
+        // Chose Ønsket omfang  - Heltid
+        driver.findElement(By.cssSelector("label.checkbox-label")).click();
+        // Click button Registration
+        ClickOnButtonRegistrationStep4();
+        // Step 5
+        // Chose Utdanningsnivå - Yrkesskole / videregående skole
+        driver.findElement(By.id("EducationLevel_chosen")).click();
+        driver.findElement(By.xpath("//*[@id='EducationLevel_chosen']/div/div/input")).sendKeys("Yrkesskole / videregående skole");
+        driver.findElement(By.xpath("//*[@id='EducationLevel_chosen']/div/ul/li/em")).click();
+        // Chose Arbeidserfaring - 1+ - 2 år  arbeidserfaring
+        driver.findElement(By.id("CareerLevel_chosen")).click();
+        driver.findElement(By.xpath("//*[@id='CareerLevel_chosen']/div/div/input")).sendKeys("1+ - 2 år  arbeidserfaring");
+        driver.findElement(By.xpath("//*[@id='CareerLevel_chosen']/div/ul/li/em")).click();
+        // Chose Kjernekompetanse
+        driver.findElement(By.id("CoreCompetence_chosen")).click();
+        driver.findElement(By.xpath("//*[@id='CoreCompetence_chosen']/ul/li/input")).sendKeys("Matematikk og naturfag");
+        driver.findElement(By.xpath("//*[@id='CoreCompetence_chosen']/div/ul/li/em")).click();
+        // Click button Registration
+        ClickOnButtonRegistrationStep5();
+        // Step 6
+        // Chose Proffesion
+        driver.findElement(By.xpath("//*[@id='bi-cat-main']/div[1]")).click();
+        Thread.sleep(1000);
+        // Scroll page up
+        ((JavascriptExecutor) driver).executeScript("window.scrollTo(0, document.head.scrollHeight)");
+        ClickOnButtonRegistrationStep6();
+        // Step 7
+        // Chose City
+        driver.findElement(By.xpath("//*[@id='bi-cat-main']/div[1]")).click();
+        Thread.sleep(1000);
+        // Scroll page up
+        ((JavascriptExecutor) driver).executeScript("window.scrollTo(0, document.head.scrollHeight)");
+        ClickOnButtonRegistrationStep7();
+        Assert.assertEquals(driver.findElement(By.cssSelector("div.sign-up-wrapper > div > div > div > div > h3")).getText(), "Bekreft din e-postadresse");
+        // Go to Page ActivationAccount
+        driver.get(baseUrl + "/Account/GetActivationLink?email=" + Email);
+        String stringFromPage = driver.findElement(By.cssSelector("pre")).getText();
+        StringBuilder sb = new StringBuilder(stringFromPage);
+        sb.deleteCharAt(sb.length() - 1);
+        sb.deleteCharAt(0);
+        String linkConfirtm = sb.toString();
+        // Go to LinkConfirm
+        driver.get(linkConfirtm);
+        try {
+            assertEquals(driver.findElement(By.cssSelector("strong")).getText(), "Velkommen til din personlige startside for jobb og karriere!");
+        } catch (Error e) {
+            verificationErrors.append(e.toString());
+        }
+        try {
+            assertEquals(driver.findElement(By.cssSelector("span.li-txt.overflow-ellipsis")).getText(), "Test3 User3");
+        } catch (Error e) {
+            verificationErrors.append(e.toString());
+        }
+        // Go to CV
+        driver.get(baseUrl + "/CV/PersonalInfo");
+        try {
+            assertEquals(driver.findElement(By.id("FirstName")).getAttribute("value"), "Test3");
+        } catch (Error e) {
+            verificationErrors.append(e.toString());
+        }
+        try {
+            assertEquals(driver.findElement(By.id("LastName")).getAttribute("value"), "User3");
+        } catch (Error e) {
+            verificationErrors.append(e.toString());
+        }
+        try {
+            assertEquals(driver.findElement(By.id("DateOfBirth")).getAttribute("value"), "07.05.1989");
+        } catch (Error e) {
+            verificationErrors.append(e.toString());
+        }
+        try {
+            assertEquals(driver.findElement(By.cssSelector("a.chosen-single > span")).getText(), "Mann");
+        } catch (Error e) {
+            verificationErrors.append(e.toString());
+        }
+        try {
+            assertEquals(driver.findElement(By.id("StreetAddress")).getAttribute("value"), "Gagarina street");
+        } catch (Error e) {
+            verificationErrors.append(e.toString());
+        }
+        try {
+            assertEquals(driver.findElement(By.id("Zip")).getAttribute("value"), "42121");
+        } catch (Error e) {
+            verificationErrors.append(e.toString());
+        }
+        try {
+            assertEquals(driver.findElement(By.id("Location")).getAttribute("value"), "Kiev");
+        } catch (Error e) {
+            verificationErrors.append(e.toString());
+        }
+        try {
+            assertEquals(driver.findElement(By.cssSelector("#Country_chosen > a.chosen-single > span")).getText(), "Ukraina");
+        } catch (Error e) {
+            verificationErrors.append(e.toString());
+        }
+        try {
+            assertEquals(driver.findElement(By.id("CellPhone")).getAttribute("value"), "+380 93 887 2305");
+        } catch (Error e) {
+            verificationErrors.append(e.toString());
+        }
+        try {
+            assertEquals(driver.findElement(By.id("Email")).getAttribute("value"), Email);
+        } catch (Error e) {
+            verificationErrors.append(e.toString());
+        }
+    }
+}
--- src/test/java/TestsCV/TestBaseCV.java
+++ src/test/java/TestsCV/TestBaseCV.java
@@ -1,28 +1,23 @@
-package Registration;
+package TestsCV;
 
 import com.thoughtworks.selenium.Selenium;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.*;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import static org.testng.Assert.*;
 
 import java.util.concurrent.TimeUnit;
 
 import static org.testng.Assert.fail;
 
-public class TestDataBaseRegistration {
+public class TestBaseCV {
 
     static protected Selenium selenium;
     static protected WebDriver driver;
     static protected String baseUrl;
-    static protected String UrlPageStep2;
-    static protected String UrlPageStep3;
-    static protected String UrlPageStep4;
-    static protected String UrlPageStep5;
-    static protected String UrlPageStep6;
-    static protected String UrlPageStep7;
-    static protected String LinkConfirtm;
-    static protected String Email1;
-    static protected String Email2;
+    static protected String Email;
     protected StringBuffer verificationErrors = new StringBuffer();
     private boolean acceptNextAlert = true;
 
@@ -30,12 +25,11 @@ public class TestDataBaseRegistration {
     public void setUp() throws Exception {
         driver = new FirefoxDriver();
         baseUrl = "http://dev.karrierestart.no";
-        Email1 = "testdd1@mail.ru";
-        Email2 = "testdd2@mail.ru";
+        Email = "testdd27@mail.ru";
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().maximize();
-
     }
+
 
     @AfterTest(alwaysRun = true)
     public void tearDown() throws Exception {
@@ -78,21 +72,27 @@ public class TestDataBaseRegistration {
             acceptNextAlert = true;
         }
     }
+
     public void ClickOnButtonRegistrationStep2() {
         driver.findElement(By.xpath("//button[@onclick=\"$('#registration_form').submit();\"]")).click();
     }
+
     public void ClickOnButtonRegistrationStep3() {
         driver.findElement(By.xpath("//button[@onclick=\"$('#registration_form').submit();\"]")).click();
     }
+
     public void ClickOnButtonRegistrationStep4() {
         driver.findElement(By.xpath("//*[@id='registration_form']/div/div[2]/div[1]/div/div[5]/div/button")).click();
     }
+
     public void ClickOnButtonRegistrationStep5() {
         driver.findElement(By.xpath("//*[@id='registration_form']/div/div[2]/div[1]/div/div[4]/div/button")).click();
     }
+
     public void ClickOnButtonRegistrationStep6() {
         driver.findElement(By.xpath("//*[@id='registration_cnt']/ul/div[5]/a")).click();
     }
+
     public void ClickOnButtonRegistrationStep7() {
         driver.findElement(By.xpath("//*[@id='registration_cnt']/div/div[5]/a")).click();
     }

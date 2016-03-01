bda5ebbfad3e095366a0616debd879032274a8e3
vladislav911 <vlad_kovtun_911@mail.ru>
2016-03-01 02:51:43 +0200

    all2step

--- src/test/java/TestsJobbønsker/BTestsStillingstyper.java
+++ src/test/java/TestsJobbønsker/BTestsStillingstyper.java
@@ -196,35 +196,94 @@ public class BTestsStillingstyper extends TestBaseJobbønsker {
     }
     @Test(priority = 9)
     public void moveWithDragAndDrop() throws Exception {
-        loggIn();
-        goToStillingstyper();
-        waitForElementPresent(By.xpath("//*[@id='employment_chosen']/a"));
-        // Close reklam
-        driver.findElement(By.xpath("//*[@id='staticad']/div/div[1]")).click();
-        scrollPageUp();
+        // Scroll page to top Items
         ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView();", driver.findElement(By.xpath("//li[2]/div")));
         ((JavascriptExecutor)driver).executeScript("scroll(0,450)");
-        Thread.sleep(2000);
-        WebElement element = driver.findElement(By.xpath("//li[2]/div"));
-        Actions builder = new Actions(driver);
-        builder.dragAndDropBy(element, 0, -80).build().perform();
-
-        Thread.sleep(10000);
-        /*refreshPage();
+        // Move 4 Top Item to 3 position
+        Actions slider = new Actions(driver)
+                .dragAndDropBy(driver.findElement(By.xpath("//li[4]/div")),0, -45);
+        slider
+                .build()
+                .perform();
+        Thread.sleep(1000);
+        // Move 1 Top Item to 5 position
+        Actions slider2 = new Actions(driver)
+                .dragAndDropBy(driver.findElement(By.xpath("//li/div")),0, 170);
+        slider2
+                .build()
+                .perform();
+        Thread.sleep(1000);
+        refreshPage();
         waitForElementPresent(By.xpath("//*[@id='employment_chosen']/a"));
-        Assert.assertEquals(driver.findElement(By.xpath("//ul[@id='topitems']/li[2]/div")).getText(), "Agent (Deltid)");
+        Assert.assertEquals(driver.findElement(By.xpath("//ul[@id='topitems']/li/div")).getText(), "Frilans (Deltid)");
+        Assert.assertEquals(driver.findElement(By.xpath("//ul[@id='topitems']/li[2]/div")).getText(), "Internship (Heltid)");
         Assert.assertEquals(driver.findElement(By.xpath("//ul[@id='topitems']/li[3]/div")).getText(), "Annet (Heltid / deltid)");
-        Assert.assertEquals(driver.findElement(By.xpath("//ul[@id='topitems']/li[4]/div")).getText(), "Internship (Heltid)");
-        Assert.assertEquals(driver.findElement(By.xpath("//ul[@id='topitems']/li[5]/div")).getText(), "Ekstrahjelp (Heltid)");
+        Assert.assertEquals(driver.findElement(By.xpath("//ul[@id='topitems']/li[4]/div")).getText(), "Ekstrahjelp (Heltid)");
+        Assert.assertEquals(driver.findElement(By.xpath("//ul[@id='topitems']/li[5]/div")).getText(), "Agent (Deltid)");
         Assert.assertEquals(driver.findElement(By.xpath("//div[@id='jobList']/div/div/div[2]/ul[2]/li/div")).getText(), "Fast ansettelse (Deltid)");
         Assert.assertEquals(driver.findElement(By.xpath("//div[@id='jobList']/div/div/div[2]/ul[2]/li[2]/div")).getText(), "Franchisetaker (Heltid / deltid)");
         Assert.assertEquals(driver.findElement(By.xpath("//div[@id='jobList']/div/div/div[2]/ul[2]/li[3]/div")).getText(), "Lærling (Deltid)");
         Assert.assertEquals(driver.findElement(By.xpath("//div[@id='jobList']/div/div/div[2]/ul[2]/li[4]/div")).getText(), "Prosjekt / Engasjement (Heltid / deltid)");
-        Assert.assertEquals(driver.findElement(By.xpath("//div[@id='jobList']/div/div/div[2]/ul[2]/li[5]/div")).getText(), "Sommerjobb / sesongjobb (Heltid)");*/
-
+        Assert.assertEquals(driver.findElement(By.xpath("//div[@id='jobList']/div/div/div[2]/ul[2]/li[5]/div")).getText(), "Sommerjobb / sesongjobb (Heltid)");
+        // Scroll page to top Items
+        ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView();", driver.findElement(By.xpath("//li[2]/div")));
+        ((JavascriptExecutor)driver).executeScript("scroll(0,450)");
+        // Move 2 Item Joblist to Top Items
+        Actions slider3 = new Actions(driver)
+                .dragAndDropBy(driver.findElement(By.xpath("//ul[2]/li[2]/div")),0, -200);
+        slider3
+                .build()
+                .perform();
+        Thread.sleep(1000);
+        Actions slider4 = new Actions(driver)
+                .dragAndDropBy(driver.findElement(By.xpath("//ul[2]/li[3]/div")),0, -205);
+        slider4
+                .build()
+                .perform();
+        Thread.sleep(1000);
+        refreshPage();
+        waitForElementPresent(By.xpath("//*[@id='employment_chosen']/a"));
+        Assert.assertEquals(driver.findElement(By.xpath("//ul[@id='topitems']/li/div")).getText(), "Lærling (Deltid)");
+        Assert.assertEquals(driver.findElement(By.xpath("//ul[@id='topitems']/li[2]/div")).getText(), "Franchisetaker (Heltid / deltid)");
+        Assert.assertEquals(driver.findElement(By.xpath("//ul[@id='topitems']/li[3]/div")).getText(), "Frilans (Deltid)");
+        Assert.assertEquals(driver.findElement(By.xpath("//ul[@id='topitems']/li[4]/div")).getText(), "Internship (Heltid)");
+        Assert.assertEquals(driver.findElement(By.xpath("//ul[@id='topitems']/li[5]/div")).getText(), "Annet (Heltid / deltid)");
+        Assert.assertEquals(driver.findElement(By.xpath("//div[@id='jobList']/div/div/div[2]/ul[2]/li/div")).getText(), "Agent (Deltid)");
+        Assert.assertEquals(driver.findElement(By.xpath("//div[@id='jobList']/div/div/div[2]/ul[2]/li[2]/div")).getText(), "Ekstrahjelp (Heltid)");
+        Assert.assertEquals(driver.findElement(By.xpath("//div[@id='jobList']/div/div/div[2]/ul[2]/li[3]/div")).getText(), "Fast ansettelse (Deltid)");
+        Assert.assertEquals(driver.findElement(By.xpath("//div[@id='jobList']/div/div/div[2]/ul[2]/li[4]/div")).getText(), "Prosjekt / Engasjement (Heltid / deltid)");
+        Assert.assertEquals(driver.findElement(By.xpath("//div[@id='jobList']/div/div/div[2]/ul[2]/li[5]/div")).getText(), "Sommerjobb / sesongjobb (Heltid)");
     }
-
-
+    @Test(priority = 10)
+    public void deleteFewItems() throws Exception {
+        scrollPageUp();
+        driver.findElement(By.xpath("//ul[@id='topitems']/li[5]/div/div/a/i")).click();
+        scrollPageUp();
+        driver.findElement(By.xpath("//div[@id='jobList']/div/div/div[2]/ul[2]/li[5]/div/a/i")).click();
+        Thread.sleep(1000);
+        refreshPage();
+        waitForElementPresent(By.xpath("//*[@id='employment_chosen']/a"));
+        assertElementNotPresent(By.xpath("//*[@id='topitems']/li[5]/div"));
+        assertElementNotPresent(By.xpath("//*[@id='jobList']/div/div/div[2]/ul[2]/li[5]/div"));
+    }
+    @Test(priority = 11)
+    public void deleteAllItems() throws Exception {
+        driver.findElement(By.xpath("//a[contains(text(),'Slett alle')]")).click();
+        driver.findElement(By.xpath("//a[contains(text(),'Ja')]")).click();
+        Thread.sleep(5000);
+        refreshPage();
+        waitForElementPresent(By.xpath("//*[@id='employment_chosen']/a"));
+        assertElementNotPresent(By.xpath("//*[@id='topitems']/li[1]/div"));
+        assertElementNotPresent(By.xpath("//*[@id='topitems']/li[2]/div"));
+        assertElementNotPresent(By.xpath("//*[@id='topitems']/li[3]/div"));
+        assertElementNotPresent(By.xpath("//*[@id='topitems']/li[4]/div"));
+        assertElementNotPresent(By.xpath("//*[@id='topitems']/li[5]/div"));
+        assertElementNotPresent(By.xpath("//*[@id='jobList']/div/div/div[2]/ul[2]/li[1]/div"));
+        assertElementNotPresent(By.xpath("//*[@id='jobList']/div/div/div[2]/ul[2]/li[2]/div"));
+        assertElementNotPresent(By.xpath("//*[@id='jobList']/div/div/div[2]/ul[2]/li[3]/div"));
+        assertElementNotPresent(By.xpath("//*[@id='jobList']/div/div/div[2]/ul[2]/li[4]/div"));
+        assertElementNotPresent(By.xpath("//*[@id='jobList']/div/div/div[2]/ul[2]/li[5]/div"));
+    }
 }
 
 
